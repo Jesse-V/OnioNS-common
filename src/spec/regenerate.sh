1e276e9d06730e4bccd261478b719c32880fc4f1
Jesse Victors <jvictors@jessevictors.com>
2016-02-21 21:49:52 -0500

    Regenerate specification during build

--- src/spec/regenerate.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-#LD_PRELOAD="/usr/local/lib/libjsonrpccpp-common.so /usr/local/lib/libjsonrpccpp-stub.so.0" jsonrpcstub rpc_spec.json --cpp-server=AbstractStubServer --cpp-client=StubClient
-
-jsonrpcstub rpc_spec.json --cpp-server=AbstractSpecServer --cpp-client=AbstractSpecClient
-mv abstractspecserver.h AbstractSpecServer.h
-mv abstractspecclient.h AbstractSpecClient.h
