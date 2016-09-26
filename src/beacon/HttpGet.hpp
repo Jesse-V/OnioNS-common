ede7fa697fd6c389d5370d3f7b73accb8fdf513f
Jesse Victors <kernelcorn@torproject.org>
2016-09-25 22:48:59 -0400

    Move CSPRNG code to beacon directory

--- src/beacon/HttpGet.hpp
+++ src/beacon/HttpGet.hpp
@@ -5,6 +5,10 @@
 #include <curl/curl.h>
 #include <string>
 
+// HMAC_SHA256(BlockHeader concat Hash(BlockHeader), Hash("OnioNS entropy
+// extractor"))
+// BlockHeader is 640 bits
+
 class HttpGet
 {
  public:
